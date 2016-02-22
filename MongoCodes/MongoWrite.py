e3c920998e1a07787f78674102e6094376b7186e
Humachine <swarun@gmail.com>
2016-02-21 18:35:42 -0800

    Finalized Json input formats and skeleton for bulk write

--- MongoCodes/MongoWrite.py
+++ MongoCodes/MongoWrite.py
@@ -5,17 +5,32 @@ def createConnection():
     client = MongoClient()
     return client
 
-def selectdb(client):
+def selectdb(client, dbname='test'):
     db = client.test
     return db
 
-def insertDocument(database, record):
-    if database=="HEAD":
+def insertDocument(db, collection, record):
+    if collection=="head":
         result = db.head.insert_one(record)
-    else if database == "BODY":
+    else if collection == "body":
         result = db.body.insert_one(record)
-    else if database == "anchors":
-        result = db.body.insert_one(record)
+    else if collection == "anchors":
+        result = db.anchors.insert_one(record)
+
     return result
 
+def bulkInsert():
+    pass
 
+'''
+url:
+    head:
+        words:frequency
+    body:
+        words:frequency
+    anchors:
+        words:frequency
+
+(url, dictionary)
+dictionary = {head: , body:, anchors: }
+'''
