84ef05b101b5281710ba8410842eb015196e1b6c
shiladitya <shiladis@uci.edu>
2016-03-12 04:22:25 -0800

    Add changes to InvIndex.py: Not tested

--- MongoCodes/Invindex.py
+++ MongoCodes/Invindex.py
@@ -1,27 +1,3 @@
-# API gives a dictioary named xyz with URL name as key and dictionary {word:frequency} of words with frequency
-# imp_words is a list of special words with stop words removed
-# wordsinDocs is a dictionary containing word as key and a dictionary of URL names:freq containig that word
-
-# def invertedIndex(xyz,imp_words[]):
-#   wordsinDocs={}
-#   for word in imp_words: #traverse through each word
-#       for document in xyz: #traverse through each document and check for the word  
-#           if word in value of document:  #traverse through the dictionary containing word entries inside that URL 
-#               wordsinDocs[word]+=document
-
-# #Swarun's version
-
-#   for doc in documents:
-#       for word in doc:
-#           if word is already there in wordDictionary:
-#               wordDictionary[urls] += url
-#           else:
-#               wordDictionary[urls] = url
-
-###########
-## FINAL ##
-###########
-
 '''
 Forward Index Entry:
 {
