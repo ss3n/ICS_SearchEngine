03f1c2f797925177abd8e6897107bdbf4e1ce1a5
shiladitya <shiladis@uci.edu>
2016-03-12 19:54:35 -0800

    Deleted redundant folder

--- ICS_SearchEngine/Crawler/Script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -u Run.py | tee missing
--- ICS_SearchEngine/Crawler/command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module load sge
-qsub -q 15day.q -M shiladis@ics.uci.edu -m beas -o ~/crawler.$JOB_ID.out -e ~/errcrawler.$JOB_ID.err Script.sh
-
