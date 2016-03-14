5c50ba2b74fae0fcf5f6876f124c6f0ffeba48cf
shiladitya <shiladis@uci.edu>
2016-03-14 13:01:25 -0700

    Trying Page Rank

--- MongoCodes/RankPage.py
+++ MongoCodes/RankPage.py
@@ -2,21 +2,12 @@ from __future__ import division
 from VitalConstants import *
 from MongoWrite import createConnection, selectDatabase
 
-incoming= {} #incoming[3] = [4] => Page 4 has a link to 3. i.e Page 3 has a backlink from 4 
-incoming[1] = [3]
-incoming[2] = [1]
-incoming[3] = [4, 1, 2]
-incoming[4] = []
-
-outgoing = {1:2, 2:1, 3:1, 4:1}
-
 DAMPING = 0.85
 d = DAMPING 
 THRESHOLD = 0.1
 
 def stable(currentPageRank, pastPageRank): 
     diff = [abs(currentPageRank[i] - pastPageRank[i]) for i in pastPageRank.keys()] 
-    print max(diff)
     if max(diff) < THRESHOLD: 
         return True 
     return False
@@ -49,13 +40,14 @@ def obtainIncomingAndOutgoingLinks():
     cursor = coll.find()
     for doc in cursor: #For each document in the collection
         url = doc[URL_DICT_KEY]     #Obtain URL from the document
-        print url
 
         #If the document does not have the anchortext attribute at all, then it has no incoming or outgoing links
         if ANCHORS_DICT_KEY in doc[CONTENT_DICT_KEY]    :
             # Obtain the incoming & Outgoing dictionaries
             out = doc[CONTENT_DICT_KEY][ANCHORS_DICT_KEY][ANCHORS_OUTGOING_DICT_KEY]
             inc = doc[CONTENT_DICT_KEY][ANCHORS_DICT_KEY][ANCHORS_INCOMING_DICT_KEY]
+            #TODO : Incoming set of URLS is missing. Use outgoing to add to outgoing dir
+            break
             if inc!= []:
                 inc = inc[0][1]
 
@@ -73,9 +65,9 @@ def obtainIncomingAndOutgoingLinks():
             outgoing[url] = 0
             incoming[url] = []
         ctr+=1
-        print ctr
+        if ctr%1000 == 0:
+            print ctr
     print 'Reading everything Done'
-    exit()
     return incoming, outgoing
 
 
@@ -103,16 +95,33 @@ def main():
     pastPageRank = {i:0 for i in incoming.keys()}
 
     it = 0
-    while not stable(currentPageRank, pastPageRank):
-        pastPageRank = currentPageRank.copy()
-        for page in incoming.keys():
-            currentPageRank[page] = (1-d)
-            some = [pastPageRank[i]/outgoing[i] if outgoing[i]>0 else 0 for i in incoming[page]]
-            currentPageRank[page]+=d*sum(some)
-        it +=1
-        print 'Iteration #', it
 
-    print currentPageRank 
+    # while not stable(currentPageRank, pastPageRank):
+    #     pastPageRank = currentPageRank.copy()
+    #     for page in incoming.keys():
+            # print it, page, incoming[page]
+            # currentPageRank[page] = (1-d)
+            # try:
+            #     some = [pastPageRank[i]/outgoing[i] if outgoing[i]>0 else 0 for i in incoming[page]]
+            # except KeyError:
+            #     print 'Debugging'
+            #     print page
+            #     print incoming[page]
+            #     print pastPageRank[i]
+            #     print outgoing[i]
+            #     print currentPageRank[page]
+            #     print
+            #     break
+            # currentPageRank[page]+=d*sum(some)
+        # it +=1
+        # print 'Iteration #', it
+ 
+    for page in incoming.keys():
+        if incoming[page] != []:
+            print page
+            print incoming[page]
+            print
+
+    # print currentPageRank 
     # writePageRanksToDatabase(currentPageRank)
-
 main()
